c2b4085b1ef77c1ad7e1ba7712469d85c44190b3
mathew mcloughlin <mathew.mcloughlin@gmail.com>
2012-09-20 22:54:21 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+var restify = require('restify');
+var connect = require('connect');
+
+
+function respond(req, res, next) {
+	res.send('hello ' + req.params.name);
+}
+
+var server = restify.createServer();
+server.use(connect.logger());
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+var static_docs_server = connect.static(__dirname + '/views');
+server.get(/\/\/*/, function(req, res, next) {
+	return static_docs_server(req, res, next);
+});
+
+server.listen(8080, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
