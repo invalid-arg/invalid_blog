dbc5fb7defba698a14381bfa1bb7cba20d30732e
mathew mcloughlin <mathew.mcloughlin@gmail.com>
2012-12-27 13:30:49 +0000

    Blog post on client side routing

--- public/js/app/router.js
+++ public/js/app/router.js
@@ -2,13 +2,14 @@ define(['underscore', 'jquery', 'sammy', 'app/routes'],
 	function(_, $, Sammy, routes) {
 		
 		var home = '#/posts';
-		var app = new Sammy();
+		var app = new Sammy('#app');
 		
 		var registerRoutes = function() {
 			_.each(routes, function(route) {
 				app.get(route.route, function() {
+					var container = this.$element(); // $('#app');
 					require([route.viewModel], function(viewModel) { 
-						$('#app').load(route.template);
+						container.load(route.template);
 					});
 				});
 			});
