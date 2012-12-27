2838e98bc247f9e968e07fd08163d4baee55af85
mathew mcloughlin <mathew.mcloughlin@gmail.com>
2012-12-27 13:32:59 +0000

    hopefully deleteing unused files

--- testing.js
+++
@@ -1,9 +0,0 @@
-var Mongolian = require('mongolian');
-var db = new Mongolian('localhost:27017/invalid_arg');
-	var posts = db.collection("posts");
-	var allPosts = '';
-	posts.find({}).toArray(function (err, posts) {
-//		allPosts += post;
-		console.log(posts)
-	});
-//	console.log(allPosts);
